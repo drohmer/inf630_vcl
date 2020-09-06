e5f6039deb93347e43da8b09de5a1debbe0fca13
Damien Rohmer <damien.rohmer@gmail.com>
2020-09-06 17:02:12 +0200

    creation inf630

---
+++ scenes/scenes.hpp
@@ -0,0 +1,13 @@
+
+#pragma once
+
+// All scenes must be included in this file
+//  If you add a new scene: its corresponding header file must be included
+//  This can be done manually or using the automatic script
+
+#include "scenes/refreshers/00_default/default.hpp"
+#include "scenes/refreshers/01_animation/interpolation_position.hpp"
+#include "scenes/refreshers/01_animation/articulated_hierarchy.hpp"
+#include "scenes/refreshers/02_simulation/example_mass_spring.hpp"
+#include "scenes/refreshers/02_simulation/cloth.hpp"
+#include "scenes/refreshers/03_skinning/skinning.hpp"
